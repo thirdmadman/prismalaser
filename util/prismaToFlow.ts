1458d44235a194ac256e35fa6f5d36584a1b5df3
thirdmadman <thirdmadman@gmail.com>
2025-07-04 07:50:58 +0200

    fix: correct build error due to type error

--- util/prismaToFlow.ts
+++ util/prismaToFlow.ts
@@ -355,13 +355,13 @@ const generateImplicitModelNodes = (
   relations: Record<string, GotModelRelations>,
 ): ModelNodeData[] => {
   const hasVirtuals = Object.values(relations).filter((rel) => rel.virtual);
-  const grouped = map(
-    (rel: GotModelRelations[]) => {
-      const fields = rel.map((r) => r.virtual!.field);
-      return { relationName: rel[0]!.dbName!, fields };
-    },
+
+  const grouped = Object.values(
     groupBy((rel) => rel.virtual!.name, hasVirtuals),
-  );
+  ).map((rel: GotModelRelations[] | undefined) => {
+    const fields = rel!.map((r) => r.virtual!.field);
+    return { relationName: rel![0]!.dbName!, fields };
+  });
 
   return Object.entries(grouped).map(([name, { relationName, fields }]) => {
     const columns: ModelNodeData["columns"] = fields.map((col, i) => ({
