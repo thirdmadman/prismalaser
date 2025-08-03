1a7df21b4d9973f120736611053350dfd74dbf68
thirdmadman <thirdmadman@gmail.com>
2025-08-03 17:40:48 +0200

    feat: add ModelNodeTableRow

---
+++ src/widgets/schema-viewer/ui/ModelNodeTableRow.tsx
@@ -0,0 +1,120 @@
+import type { ReactNode } from 'react';
+import cc from 'classcat';
+import { Handle, Position, useReactFlow, useStoreApi } from 'reactflow';
+
+import {
+  generateEnumEdgeTargetHandleId,
+  generateRelationEdgeSourceHandleId,
+  generateRelationEdgeTargetHandleId,
+} from '@/shared/lib/prismaToFlow';
+import type { IModelNodeData, IModelNodeDataColumn } from '@/shared/lib/types';
+
+import styles from './Node.module.scss';
+
+const isRelationed = ({ relationData }: IModelNodeDataColumn) => !!relationData?.side;
+
+interface IModelNodeTableRowProps {
+  data: IModelNodeData;
+  sourceTableColumn: IModelNodeDataColumn;
+}
+
+export function ModelNodeTableRow({ data, sourceTableColumn }: IModelNodeTableRowProps) {
+  const store = useStoreApi();
+  const { setCenter, getZoom } = useReactFlow();
+
+  const focusNode = (nodeId: string) => {
+    const { nodeInternals } = store.getState();
+    const nodes = Array.from(nodeInternals).map(([, node]) => node);
+
+    if (nodes.length > 0) {
+      const node = nodes.find((iterNode) => iterNode.id === nodeId);
+
+      if (!node) return;
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const x = node.position.x + node.width! / 2;
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const y = node.position.y + node.height! / 2;
+      const zoom = getZoom();
+
+      setCenter(x, y, { zoom, duration: 1000 });
+    }
+  };
+
+  const reled = isRelationed(sourceTableColumn);
+  let targetHandle: ReactNode | null = null;
+  let sourceHandle: ReactNode | null = null;
+
+  if (sourceTableColumn.kind === 'enum') {
+    const handleId = generateEnumEdgeTargetHandleId(data.name, sourceTableColumn.name);
+    targetHandle = (
+      <Handle
+        key={handleId}
+        className={cc([styles.handle, styles.left])}
+        type="target"
+        id={handleId}
+        position={Position.Left}
+        isConnectable={false}
+      />
+    );
+  } else if (sourceTableColumn.relationData) {
+    const targetHandleId = generateRelationEdgeTargetHandleId(
+      data.name,
+      sourceTableColumn.relationData.name,
+      sourceTableColumn.name
+    );
+    const sourceHandleId = generateRelationEdgeSourceHandleId(
+      data.name,
+      sourceTableColumn.relationData.name,
+      sourceTableColumn.name
+    );
+
+    targetHandle =
+      sourceTableColumn.relationData.side === 'target' ? (
+        <Handle
+          key={targetHandleId}
+          className={cc([styles.handle, styles.left])}
+          type="target"
+          id={targetHandleId}
+          position={Position.Left}
+          isConnectable={false}
+        />
+      ) : null;
+    sourceHandle =
+      sourceTableColumn.relationData.side === 'source' ? (
+        <Handle
+          key={sourceHandleId}
+          className={cc([styles.handle, styles.right])}
+          type="source"
+          id={sourceHandleId}
+          position={Position.Right}
+          isConnectable={false}
+        />
+      ) : null;
+  }
+
+  return (
+    <tr key={sourceTableColumn.name} className={styles.row} title={sourceTableColumn.documentation}>
+      <td className="font-mono font-semibold border-t-2 border-r-2 border-gray-300">
+        <button
+          type="button"
+          className={cc(['relative', 'p-2', { 'cursor-pointer': reled }])}
+          onClick={() => {
+            if (!reled) return;
+            focusNode(sourceTableColumn.type);
+          }}
+        >
+          {sourceTableColumn.name}
+          {targetHandle}
+        </button>
+      </td>
+      <td className="p-2 font-mono border-t-2 border-r-2 border-gray-300">{sourceTableColumn.displayType}</td>
+      <td className="font-mono border-t-2 border-gray-300">
+        <div className="relative p-2">
+          {sourceTableColumn.defaultValue ?? ''}
+          {sourceHandle}
+        </div>
+      </td>
+    </tr>
+  );
+}
